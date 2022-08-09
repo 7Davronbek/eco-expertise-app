bb7647669b920d03b4b1a9c4ba602dca8fcdc061
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-08-09 17:16:39 +0500

    eco-expertise-app

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1>Lectus elit odio nisl nec sit proin.</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
