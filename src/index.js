348f2464d20408cc39667dfd294b860b3398347e
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-11-17 17:47:33 +0500

    eco-expertise-app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './style/main.scss'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/main.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HashRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HashRouter>
 );
-
-reportWebVitals();
